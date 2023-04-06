6d42856437be28754174f745041980744c03dd60
Kelb-hash <125036118+Kelb-hash@users.noreply.github.com>
2023-04-06 22:03:05 +0100

    Update and rename new.py to bool3.py

--- pythonbooleans/bool3.py
+++ pythonbooleans/bool3.py
@@ -1,6 +1,8 @@
-current_year = 2023 #you can also get the current age using the datetime module
 
-def is_student_adult():
+
+current_year = 2023 #global variable
+
+def is_student_adult(): #this function calculates the age of student and returns true or false
     name = input("input your name: ")
     date_of_birth = int(input("input year of birth: "))
     age = current_year  -  date_of_birth
